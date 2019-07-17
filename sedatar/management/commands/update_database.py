8edbb082e7e476ef1d2f6f60d24fdd2adab76d2c
Mathias Uhlenbrock <mu@munich-innovation.com>
2019-07-17 21:24:55 +0200

    Code cleanup.

--- sedatar/management/commands/update_database.py
+++ sedatar/management/commands/update_database.py
@@ -1,11 +1,7 @@
-import csv
-from subprocess import call
 from django.core.management.base import BaseCommand
 
-class Command(BaseCommand):
 
+class Command(BaseCommand):
     def handle(self, *args, **options):
-        # Update the database.
-        # --------------------
-        execfile("sedatar/scripts/python/update.py")
-        execfile("astronomical_database/scripts/python/update.py")
+        exec(open('sedatar/scripts/python/update.py').read())
+        exec(open('astronomical_database/scripts/python/update.py').read())
