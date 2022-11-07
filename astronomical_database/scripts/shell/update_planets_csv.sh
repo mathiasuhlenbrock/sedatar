bcaed42e3f4cdf805b1b748eb347b73b01a7c8ff
Mathias Uhlenbrock <mathias.uhlenbrock@gmail.com>
2022-11-07 19:27:30 +0100

    Small improvements.

--- astronomical_database/scripts/shell/update_planets_csv.sh
+++ astronomical_database/scripts/shell/update_planets_csv.sh
@@ -3,4 +3,6 @@
 DATE_STRING=$(date +%Y-%m-%d)
 DATA_DIR=astronomical_database/data/csv/planets
 wget "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+disc_year,hostname,pl_dens,pl_letter,pl_massj,pl_orbper,pl_orbsmax,pl_radj,st_lum,st_rad,st_spectype,sy_bmag,sy_dist,sy_vmag+from+ps+where+default_flag=1&format=csv" -O "${DATA_DIR}/planets.${DATE_STRING}.csv"
+echo "Update file: 'planets.csv'."
 cp ${DATA_DIR}/planets."${DATE_STRING}".csv ${DATA_DIR}/planets.csv
+echo "Done."
