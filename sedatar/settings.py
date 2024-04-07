526104bdf31e1d29f97d414db4bda12e93cb9a11
Mathias Uhlenbrock <mathias.uhlenbrock@gmail.com>
2024-04-07 13:26:15 +0200

    Removed secrets from repository.

--- sedatar/settings.py
+++ sedatar/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-s^7m%fx+*no=(*_r-&kzwwllj_4^)=lpv1gy$e)*r65j_jyt9'
+#SECRET_KEY = '-s^7m%fx+*no=(*_r-&kzwwllj_4^)=lpv1gy$e)*r65j_jyt9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
